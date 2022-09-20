fe505f5930160d517beb66a4cd2d18a341dd47d7
ilya <ipbsmk@gmail.com>
2022-09-20 11:23:40 +0400

    нашли случайное число из отрезка [10, 99] и показывает

--- task09_chislo_iz_otrezka/Program.cs
+++ task09_chislo_iz_otrezka/Program.cs
@@ -14,13 +14,49 @@
 // else Console.WriteLine($"Наибольшая цифра числа {number} = {secondDigit}");
 
 
+// int number = new Random().Next(10, 100);
+// Console.WriteLine($"Слуайное число из отрезка 10 99 =>  {number}");
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit == secondDigit) Console.WriteLine($"Цифвры равны");
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"{maxDigit}");
 
 
-// int maxDigit = firstDigit;
+
 // if (firstDigit < secondDigit) maxDigit = secondDigit;
-
 // int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
 
+
+
+
+// int MaxDigit(int num)
+// {
+//     int firstDigit = num / 10;
+//     int secondDigit = num % 10;
+//     if (firstDigit == secondDigit) return 0;
+//     return firstDigit > secondDigit ? firstDigit : secondDigit;
+// }
+// int number = new Random().Next(10, 100);
+// int result = MaxDigit(number);
+// Console.WriteLine($"Наибольшая цифра числа {number} = {result}");
+
+
+// int MaxDigit(int num)
+// {
+//     int firstDigit = num / 10;
+//     int secondDigit = num % 10;
+//     if (firstDigit == secondDigit) return 0;
+//     return firstDigit > secondDigit ? firstDigit : secondDigit;
+// }
+// int number = new Random().Next(10, 100);
+// int maxDigit = MaxDigit(number);
+// string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры равны";
+// Console.WriteLine($"Наибольшая цифра числа {number} = {result}");
+
+
+// if (firstDigit > secondDigit) return firstDigit;
+// return secondDigit;
 
 
 
@@ -28,32 +64,19 @@ int MaxDigit(int num)
 {
     int firstDigit = num / 10;
     int secondDigit = num % 10;
-    if (firstDigit > secondDigit) return firstDigit;
-    return secondDigit;
+    // if (firstDigit > secondDigit) return firstDigit;
+    // return secondDigit;
+    if(IsEqualDigits(firstDigit, secondDigit)) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
 }
+
+bool IsEqualDigits(int num1, int num2)
+{
+    return num1 == num2;
+}
+
 int number = new Random().Next(10, 100);
-int result = MaxDigit(number);
-Console.WriteLine($"Наибольшая цифра числа {number} = {result}");
+int maxDigit = MaxDigit (number);
 
-
-
-// nt MaxDigit(int num)
-// {
-//     int firstDigit = num / 10;
-//     int secondDigit = num % 10;
-//     // if (firstDigit > secondDigit) return firstDigit;
-//     // return secondDigit;
-//     if(IsEqualDigits(firstDigit, secondDigit)) return 0;
-//     return firstDigit > secondDigit ? firstDigit : secondDigit;
-// }
-
-// bool IsEqualDigits(int num1, int num2)
-// {
-//     return num1 == num2;
-// }
-
-// int number = new Random().Next(10, 100);
-// int maxDigit = MaxDigit (number);
-
-// string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры равны";
-// Console.WriteLine($"Наибольшая цифра числа {number} = {maxDigit}");
+string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры равны";
+Console.WriteLine($"Наибольшая цифра числа {number} = {maxDigit}");
