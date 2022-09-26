f9b7c84bc640c266b7135b2a3f67d814e33d73e9
ilya <ipbsmk@gmail.com>
2022-09-26 13:32:02 +0400

    решили задаи с семинара

---
+++ task24_summaCHISELit1doA/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumResult = SumNumbers(number);
+Console.WriteLine($"Сумма чисел о 1 до {number} = {sumResult}");
+
+int SumNumbers(int num)
+{
+    int sum = default;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+
+
+
+// Console.WriteLine("Введите число:     ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int summResult = SummNumbers(number);
+// System.Console.WriteLine($"{summResult}");
+
+// int SummNumbers(int num)
+// {
+//     int summ = default;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         summ = summ + i;
+//     }
+//     return summ;
+// }
+
+
+// Console.WriteLine("Введите число:     ");                // второй вариант решения
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int SummNumbers2(int num)
+// {
+//     int summ = default;
+//     int counter = 1;
+//     while (counter <= num)
+//     {
+//         sunn = summ + counter;
+//         counter ++;
+//     }
+//     return summ;
+// }
---
+++ task30_vivod_massiva/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int[] array1 = new int[8] {1,2,3,4,5,6,7,8};
+// int[] array2 = {1,2,3,4,5,6,7,8};
+// var array3 = new int [8]; 
+
+
+int[] array = new int[8];
+GetArray(array);
+PrintArray(array);
+
+
+void GetArray(int[] arr)
+{
+    int length = arr.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
