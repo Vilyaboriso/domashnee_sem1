0d05097ce283ef011f4a605eed59a4fe92692935
ilya <ipbsmk@gmail.com>
2022-09-29 19:39:50 +0400

    вывод массива из 8 элементов

---
+++ task29_vivodMASSIVA/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] array1 = new int[8] {1,2,3,4,5,6,7,8};
+
+int[] array = new int[8];
+GetArray(array);
+PrintArray(array);
+
+
+void GetArray(int[] arr)
+{
+    int length = arr.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(1, 99);
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
