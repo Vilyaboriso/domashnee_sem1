225e9d395893e2a5f6fe91ffdf7c8496584c5aba
ilya <ipbsmk@gmail.com>
2022-09-29 19:23:04 +0400

    решили 25 и 27 с горем

---
+++ task25_chisloVstepeni/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+
+if (number2 == 0)
+    Console.WriteLine($"Возведение первого числа {number1} в натуральную степень второго числа {number2} = 1");
+
+if (number2 > 0)
+{
+    double Degree(int num1, int num2)
+    {
+        double degree = num1 * num1;
+        while (counter < num2)
+        {
+            degree = degree * num1;
+            counter++;
+        }
+        return degree;
+    }
+    double degreeResult = Degree(number1, number1);
+    Console.WriteLine($"Возведение первого числа {number1} в натуральную степень второго числа {number2} = {degreeResult}");
+}
+
+else Console.WriteLine("Введите второе число положительное");
+
+
+
+
+
+
---
+++ task27_summCZIFRvSHISLE/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+ 
+Очень долго пытался решить задачу, в итоге в 21 строке заменил i++ на i = i++ и только после этоого заработало.
+Почему если оставить i++ то не все значения правильно считает?
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int res = SumNumbers(number);
+Console.WriteLine(Math.Abs(res));
+
+int SumNumbers(int num)
+{
+    int sum = default;
+
+    for (int i = 1; i <= Math.Abs(num); i = i++)
+    {
+        sum = sum + num % 10 + num /10 % 10 + num /100 % 10;
+        num = num /1000;
+    }
+    
+    return sum;
+    
+}
+
